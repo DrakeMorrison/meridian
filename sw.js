15ff5d734c4fde79def57607039cc6d9d89e4a44
DrakeMorrison <36492596+DrakeMorrison@users.noreply.github.com>
2026-05-20 17:33:35 -0700

    removed annoying notifications

--- sw.js
+++ sw.js
@@ -25,15 +25,3 @@ self.addEventListener('fetch', (e) => {
       .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
-
-self.addEventListener('notificationclick', (e) => {
-  e.notification.close();
-  e.waitUntil(
-    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
-      for (const client of clientList) {
-        if ('focus' in client) return client.focus();
-      }
-      if (self.clients.openWindow) return self.clients.openWindow('./');
-    })
-  );
-});
